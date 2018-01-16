b2cea825720975ccd5ca1ab30533edd4f394bf33
Craig McMahon <craigthebrit@gmail.com>
2018-01-16 11:24:44 +0100

    Initial Commit

---
+++ LocaleTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocaleTest
+//
+//  Created by Mcmahon, Craig on 1/16/18.
+//  Copyright © 2018 Marktplaats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
